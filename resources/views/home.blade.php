cb9e37e7a65391de258b353166189b32791a242a
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2018-11-23 10:18:11 +0100

    updated home.blade.php, this will change to dashboard.blade.php soon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! THIS WILL CHANGE SOON!
                 </div>
             </div>
         </div>
