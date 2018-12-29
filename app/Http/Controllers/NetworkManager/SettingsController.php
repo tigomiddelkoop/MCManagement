fb718525c357fb95fa068f59420d4b69a34d07fd
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2018-12-30 00:53:23 +0100

    Started working on the settings page! It looks like nothing at the moment!, But ut will turn into something more later this day.

---
+++ app/Http/Controllers/NetworkManager/SettingsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\NetworkManager;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SettingsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+
+}
